c1943b8b602d90296d5cae99b074a4af5de0e806
nek0peko <sakura993625307@gmail.com>
2022-12-14 00:50:15 +0800

    fix: 后端解决跨域问题

---
+++ back-end/src/main/java/pers/nek0peko/datas/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package pers.nek0peko.datas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 解决跨域问题
+ *
+ * @author nek0peko
+ * @date 2022/12/14
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
