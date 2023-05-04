0281d1a910833824aff766b259b75b16a5cea327
nek0peko <sakura993625307@gmail.com>
2023-05-04 21:12:55 +0800

    update

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: '0.0.0.0',
+    client: {
+      webSocketURL: 'ws://0.0.0.0/ws',
+    },
     allowedHosts: [
-        '.nek0peko.com'
+      '.nek0peko.com'
     ]
   }
 })
