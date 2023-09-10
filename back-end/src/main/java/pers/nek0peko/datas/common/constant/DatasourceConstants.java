fb1958de34574b03d612be32d7b73cf0b136eaee
nek0peko <sakura993625307@gmail.com>
2023-09-10 15:44:00 +0800

    update

---
+++ back-end/src/main/java/pers/nek0peko/datas/common/constant/DatasourceConstants.java
@@ -0,0 +1,15 @@
+package pers.nek0peko.datas.common.constant;
+
+/**
+ * DatasourceConstants
+ *
+ * @author nek0peko
+ * @date 2023/05/02
+ */
+public class DatasourceConstants {
+
+    public static final String GREENPLUM_CONFIG_JSON = "{\"host\": \"xxx.xxx.xxx.xxx\", \"port\": \"xxxx\", " +
+            "\"database\": \"postgres\", \"schema\": \"sync\", \"username\": \"xxxxxxxx\", \"password\": \"xxxxxxxx\", " +
+            "\"jdbc\": \"characterEncoding=UTF-8&connectTimeout=5000&useSSL=false&allowPublicKeyRetrieval=true\"}";
+
+}
