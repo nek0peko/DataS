bf81f4c16d004ed3ece1d5a266116b18e0a6c2df
nek0peko <sakura993625307@gmail.com>
2023-09-12 21:05:13 +0800

    update: remove sensative info in git history

---
+++ back-end/src/main/java/pers/nek0peko/datas/common/constant/Constants.java
@@ -0,0 +1,15 @@
+package pers.nek0peko.datas.common.constant;
+
+/**
+ * 常量
+ *
+ * @author nek0peko
+ * @date 2023/05/02
+ */
+public class Constants {
+
+    public static final int TWO = 1 << 1;
+
+    public static final int SIXTEEN = 1 << 4;
+
+}
